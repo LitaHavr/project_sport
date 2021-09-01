0638da0562791e809b29bc69f33f2d267aff0ccf
lolita <lolitagavr@gmail.com>
2021-09-01 12:14:09 +0200

    4 blocks plus slick lider

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        speed: 1200,
+
+        prevArrow: "<button type=\"button\" class=\"slick-prev\"><img src='img/arrows/left.png'></button>",
+        nextArrow: "<button type=\"button\" class=\"slick-next\"><img src='img/arrows/right.png'></button>",
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                   dots: true,
+                   arrows:false
+                }
+           }
+        ]
+
+    });
+});
